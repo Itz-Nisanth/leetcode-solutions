e41135c95a8164ecdad16d44bc407106d82f665c
Nisanth <itz.nisanth@gmail.com>
2026-08-09 00:13:36 +0530

    array-two sum

--- Array/Two_Sum.py
+++ Array/Two_Sum.py
@@ -43,11 +43,8 @@ class Solution(object):
                 j = nums.index(need)
                 if i != j:
                     return i, j
-
+        
 s = Solution()
-
-print(s.twoSum([2, 7, 11, 15], 9))
-print(s.twoSum([3, 2, 4], 6))
-print(s.twoSum([3, 3], 6))
-
-
+print(s.twoSum([2,7,11,15], 9))
+print(s.twoSum([3,2,4], 6))
+print(s.twoSum([3,3], 6))
